5d42375685cc8016a3bff641f6a3189fc8b6df9e
Harman <harman5@sheridancollege.ca>
2026-03-10 20:11:09 -0400

    Added the basic Task module and adding new tasks function;

--- modules/test.tsx
+++ modules/test.tsx
@@ -1,10 +1,33 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './test.module.css';
 
 // Your Test Starts Here
+type Task = {
+    id : string,
+    title : string,
+};
 
 export default function TaskManager(): JSX.Element {
+    const [title,setTitle] = useState("");
+    const [task,setTask] = useState<Task[]>([]);
+
+    function addTask() {
+        const trimmedTitle = title.trim();
+
+        if (!trimmedTitle){
+            return;
+        }
+
+        const newTask : Task = {
+            id : crypto.randomUUID(),
+            title : trimmedTitle
+        }
+
+        setTask([newTask,...task]);
+        setTitle("");
+    }
+
     return <div className={styles.container}>
         
     </div>;
